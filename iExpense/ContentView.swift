1fd6db8e8af126e0e35dc8006f5a69ceb58c784c
Kitwana Akil <kakil@me.com>
2023-07-15 17:52:00 -0400

    Tutorial 3 complete.

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,21 +7,30 @@
 
 import SwiftUI
 
-class User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    let name: String
+    
+    var body: some View {
+        Button("Dismiss") {
+            dismiss()
+        }
+    }
 }
 
 struct ContentView: View {
     
-    @State private var user = User()
+    @State private var showingSheet = false
     
     var body: some View {
         VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-            
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+            Button("Show Sheet") {
+                showingSheet.toggle()
+            }
+            .sheet(isPresented: $showingSheet) {
+                SecondView(name: "@kakil")
+            }
         }
         .padding()
     }
