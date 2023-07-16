1f7ec9e4bcc9ff695256ba53facf3d77e853e806
Kitwana Akil <kakil@me.com>
2023-07-15 21:15:24 -0400

    Completed the deleting items tutorial.

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,23 +7,37 @@
 
 import SwiftUI
 
-class User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
-}
 
 struct ContentView: View {
     
-    @State private var user = User()
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-            
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .navigationTitle("onDelete()")
+            .toolbar {
+                EditButton()
+            }
         }
-        .padding()
+        
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
